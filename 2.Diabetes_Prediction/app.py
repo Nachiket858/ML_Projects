7fcf80693b0885397870607e7a15ab78a551f00c
Nachiket Shinde <nachiketshinde2004@gmail.com>
2024-12-25 21:51:56 +0530

    model   change  to SVC fot the accuracy

--- 2.Diabetes_Prediction/app.py
+++ 2.Diabetes_Prediction/app.py
@@ -6,11 +6,11 @@ warnings.filterwarnings("ignore")
 app = Flask(__name__)
 
 # Load the model and scaler
-with open(r'E:\Python\Myenv\Data_Science\ML_Projects\2.Diabetes_Prediction\Models\model.pkl', 'rb') as file:
-    model = pickle.load(file)
+with open(r'E:\Python\Myenv\Data_Science\ML_Projects\2.Diabetes_Prediction\Models\svc_classifire.pkl', 'rb') as file:
+    svc_classifire = pickle.load(file)
 
-with open(r'E:\Python\Myenv\Data_Science\ML_Projects\2.Diabetes_Prediction\Models\Scalar.pkl', 'rb') as file:
-    scalar = pickle.load(file)
+# with open(r'E:\Python\Myenv\Data_Science\ML_Projects\2.Diabetes_Prediction\Models\Scalar.pkl', 'rb') as file:
+#     scalar = pickle.load(file)
 
 @app.route('/')
 def home():
@@ -31,10 +31,10 @@ def predict():
 
         # Prepare the data for prediction
         data = [[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree, age]]
-        newdata = scalar.transform(data)  # Scale the data using the scalar
+        # newdata = scalar.transform(data)  # Scale the data using the scalar
 
         # Get the prediction
-        result = model.predict(newdata)
+        result = svc_classifire.predict(data)
 
         # Render the result on the prediction page
         if result == 1:
